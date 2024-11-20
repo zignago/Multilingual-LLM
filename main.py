9c9218ffa78510b241e395cca5e70da3f6e538e4
zignago <gianzignago@gmail.com>
2024-11-19 21:36:29 -0600

    added help output and changed supported_llm to map

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import argparse
 from src.config import SUPPORTED_LLM_MODELS
 
 def main():
+    models_formatted_output = "\n".join(
+        [f"\n{category.upper()}:\n   - " + "\n   - ".join(models) for category, models in SUPPORTED_LLM_MODELS.items()]
+    )
+
     parser = argparse.ArgumentParser(description="Multilingual LLM Keyword Extraction")
     parser.add_argument("--languages", nargs="+", default=["german"], help="Specify one or more target languages.")
     parser.add_argument("--limit", type=int, default=3, help="Limit to top n keywords.")
-    parser.add_argument("--model", type=str, default="gpt-3.5-turbo", help=f"Specify the LLM model to run. Current supported models:\n{''.join([f' - {model}\n' for model in SUPPORTED_LLM_MODELS])}")
+    parser.add_argument("--model", type=str, default="gpt-3.5-turbo", help=f"Specify the LLM model to run. Current supported models:\n{models_formatted_output}\n")
     parser.add_argument("--subset", type=int, default=20, help="Amount of entries in the dataset to use. Default=20")
     parser.add_argument("--repeat", type=int, default=1, help="Specify the number of times to run each query.")
     parser.add_argument("--output", type=str, help="Optional custom output filename (without path) for JSON results.")
     args = parser.parse_args()
     
     if args.model not in SUPPORTED_LLM_MODELS:
-        print(f"Error: unsupported model '{args.model}'\nCurrent supported models:\n{''.join([f' - {model}\n' for model in SUPPORTED_LLM_MODELS])}")
+        print(f"ERROR -- unsupported model '{args.model}'\nCurrent supported models:\n{models_formatted_output}\n")
         return 
     
     print(f"Running with the following parameters:\n"
--- src/config.py
+++ src/config.py
@@ -91,19 +91,60 @@ HARDCODE_TRANSLATIONS = {
 
 # Models that have been tested and verified to work with this tool
 # Info on GPT models: https://platform.openai.com/docs/models
-SUPPORTED_LLM_MODELS = [
-    "gpt-4o",
-    "gpt-4o-2024-08-06",
-    "gpt-4o-2024-05-13",
-    "gpt-4o-mini",
-    "gpt-4o-mini-2024-07-18",
-    "gpt-4-turbo",
-    "gpt-4-turbo-preview",
-    "gpt-4-0125-preview",
-    "gpt-4-1106-preview",
-    "gpt-4",
-    "gpt-4-0613",
-    "gpt3.5-turbo",
-    "gpt-3.5-turbo-1106",
-    "gpt-3.5-turbo-0125"
-]
+SUPPORTED_LLM_MODELS = {
+    "gpt": [
+        "gpt-4o",
+        "gpt-4o-2024-08-06",
+        "gpt-4o-2024-05-13",
+        "gpt-4o-mini",
+        "gpt-4o-mini-2024-07-18",
+        "gpt-4-turbo",
+        "gpt-4-turbo-preview",
+        "gpt-4-0125-preview",
+        "gpt-4-1106-preview",
+        "gpt-4",
+        "gpt-4-0613",
+        "gpt3.5-turbo",
+        "gpt-3.5-turbo-1106",
+        "gpt-3.5-turbo-0125"
+    ],
+    "llama": [
+        "llama-2-7b", 
+        "llama-2-13b", 
+        "llama-2-70b"
+    ]
+}
+
+
+# LLAMA Models
+# llama3.2-11b-vision
+# llama3.2-1b
+# llama3.2-3b
+# llama3.2-90b
+# llama3.1-405b
+# llama3.1-70b
+# llama3.1-8b
+# llama3-70b
+# llama3-8b
+# gemma2-27b
+# gemma2-9b
+# mixtral-8x22b
+# mixtral-8x22b-instruct
+# mixtral-8x7b-instruct
+# mistral-7b
+# mistral-7b-instruct
+# llama-7b-32k
+# llama2-13b
+# llama2-70b
+# llama2-7b
+# Nous-Hermes-2-Mixtral-8x7B-DPO
+# Nous-Hermes-2-Yi-34B
+# Qwen1.5-0.5B-Chat
+# Qwen1.5-1.8B-Chat
+# Qwen1.5-110B-Chat
+# Qwen1.5-14B-Chat
+# Qwen1.5-32B-Chat
+# Qwen1.5-4B-Chat
+# Qwen1.5-72B-Chat
+# Qwen1.5-7B-Chat
+# Qwen2-72B-Instruct
